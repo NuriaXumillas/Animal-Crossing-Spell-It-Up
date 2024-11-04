e4ad3dc2befb073fc730ea5c30ec5a2203fb972a
Nuria <tu.email@ejemplo.com>
2024-11-04 21:37:26 +0100

    todas las palabras disponibles

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -8,23 +8,23 @@ const KEY_LEFT = 37;
 
 const words = [
    // Nivel de dificultad 1:
-// { spanish: "ALMIBAR", english: "SYRUP", difficulty: 1 },
-// { spanish: "DULCE", english: "SWEET", difficulty: 1 },
-// { spanish: "BISAGRA", english: "HINGE", difficulty: 1 },
-// { spanish: "CENCERRO", english: "BELL", difficulty: 1 },
-{ spanish: "JENGIBRE", english: "GINGER", difficulty: 1 },
+{ spanish: "ALMIBAR", english: "SYRUP", difficulty: 1 },
+{ spanish: "DULCE", english: "SWEET", difficulty: 1 },
+{ spanish: "BISAGRA", english: "HINGE", difficulty: 1 },
+{ spanish: "CENCERRO", english: "BELL", difficulty: 1 },
+{ spanish: "MAR", english: "SEA", difficulty: 1 },
 
 // Nivel de dificultad 2
-// { spanish: "FLAMENCO", english: "FLAMINGO", difficulty: 2 },
-// { spanish: "CANGREJO", english: "CRAB", difficulty: 2 },
-// { spanish: "HURACÁN", english: "HURRICANE", difficulty: 2 },
-{ spanish: "CIGÜEÑA", english: "STORK", difficulty: 2 },
-// { spanish: "GRANIZO", english: "HAILSTONES", difficulty: 2 },
+{ spanish: "FLAMENCO", english: "FLAMINGO", difficulty: 2 },
+{ spanish: "CANGREJO", english: "CRAB", difficulty: 2 },
+{ spanish: "HURACÁN", english: "HURRICANE", difficulty: 2 },
+{ spanish: "OLA", english: "WAVE", difficulty: 2 },
+{ spanish: "GRANIZO", english: "HAILSTONES", difficulty: 2 },
 
 // Nivel de dificultad 3
-// { spanish: "HIPOCAMPOS", english: "SEAHORSES", difficulty: 3 }, 
-// { spanish: "QUIMERA", english: "CHIMERA", difficulty: 3 },
-{ spanish: "PEREGRINO", english: "PILGRIM", difficulty: 3 },
-// { spanish: "UROBORO", english: "UREBOROS", difficulty: 3 },
-// { spanish: "ZURRÓN", english: "SADDLEBAG", difficulty: 3 }
+{ spanish: "HIPOCAMPOS", english: "SEAHORSES", difficulty: 3 }, 
+{ spanish: "QUIMERA", english: "CHIMERA", difficulty: 3 },
+{ spanish: "JENGIBRE", english: "GINGER", difficulty: 3 },
+{ spanish: "UROBORO", english: "UREBOROS", difficulty: 3 },
+{ spanish: "ZURRÓN", english: "SADDLEBAG", difficulty: 3 }
 ]
